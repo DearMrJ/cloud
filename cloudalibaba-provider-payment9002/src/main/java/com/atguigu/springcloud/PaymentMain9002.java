786f50b1b611bf62636d40a2368c4e1bc258c94b
DearMrJ <285692983@qq.com>
2020-03-31 23:21:47 +0800

    nacos初体验之服务发现

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/PaymentMain9002.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lynch
+ * @date 2020/3/31 23:02
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+}
